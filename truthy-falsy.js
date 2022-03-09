ffdd29396e674aeeba479c9f90d38b3526816951
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 15:25:06 -0300

    Valores que representam true e false

---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+//boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+//valores considerados true ou false
+// 0 -> false
+// 1 -> true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null -> vazio -> objeto
+
+let variavelNula = null;
+let variavelUndefined;
+
+console.log(typeof variavelUndefined);
+console.log(typeof variavelNula);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
