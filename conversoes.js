cdeebc229aa15cf230baa2b3899ba012f98320c1
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 15:53:55 -0300

    Ainda sobre as conversões no JavaScript

--- conversoes.js
+++ conversoes.js
@@ -17,3 +17,11 @@ console.log(numero + numeroString);
 //String()
 
 console.log(numero + Number(numeroString));
+
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));
+
+// o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString()); 
