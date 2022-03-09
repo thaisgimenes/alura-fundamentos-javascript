f776e6459e96aff94159d8f3f70d92be687b2d57
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 18:32:10 -0300

    Trabalhando com comparações e o operador ternario no JavaScript

---
+++ comparacoes.js
@@ -0,0 +1,10 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// === (comparação explicita)
+
+console.log(numero === texto);
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= idadeMinima){
+    console.log("Cerveja");
+} else{
+    console.log("Suco");
+}
+
+//Três operadores na linha: ternario
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco");
