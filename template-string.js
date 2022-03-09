cdc3fba04712b738642927abbc238ea8783f09eb
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 18:41:40 -0300

    Aprendendo o uso do template string

---
+++ template-string.js
@@ -0,0 +1,14 @@
+const nome = "Ju";
+const idade = 2021-1981;
+const cidadeDeNascimento = "São Paulo";
+
+const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + ", nasci em " + cidadeDeNascimento;
+
+console.log(apresentacao);
+
+//Template String
+
+const apresentacaoTemplate = `Meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidadeDeNascimento}`;
+
+console.log(apresentacaoTemplate);
+
