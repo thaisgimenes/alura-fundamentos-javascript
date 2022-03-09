52b4423402eb6b24707b49e1b5a2f3f27f48a975
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 13:16:50 -0300

    Tipos primitivos do JavaScript

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// Tipo Number
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+//Ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+//NaN -> Not A Number
+
+const alura = "Alura";
+
+console.log(alura + primeiroNumero);
+
---
+++ type-string.js
@@ -0,0 +1,31 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = 'senhaSuperSegura#56';
+const stringDeNumeros = '34567';
+
+const citacao = 'O Leo disse "Oi!"';
+console.log(citacao);
+
+const frase = "Meu nome é ";
+const meuNome = "Thais";
+
+//Concateção (+)
+
+console.log(frase + meuNome);
+
+//Metodo string nativo para transformar todas as letras em minusculo
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//Propriedade para contar quantas letras tem uma string
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
+//Diferença entra métodos e propriedades ?
+
+//Template String ou template literal
+
