17574044e47d193fd37e467ea1c3c2ca9593b50a
thaisgimenes <thaisgimenes@outlook.com.br>
2022-03-09 22:46:49 -0300

    Escrevendo funções no JavaScript

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+//Declarando a função
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+//Executando a função
+
+imprimeTexto("Oi");
+imprimeTexto("Olá, de novo!");
+
+//Três formas de escrever funções
+
+function soma(){
+    return 2 + 2;
+}
+
+console.log(soma());
+imprimeTexto(soma());
