8c54c73301fc5dcf4df1316b335b89764bbf0e15
thaisgimenes <thaisgimenes@outlook.com.br>
2022-04-11 19:08:53 -0300

    Aprendendo sobre objetos no Javascript

---
+++ objetos.js
@@ -0,0 +1,11 @@
+//Array 
+const listaCPFs = ["123548988", "1256844466", "45822213266"];
+
+//Objetos são informações chaves relacionadas, abstrações do mundo real
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "4521336987",
+    email: "andre@email.com"
+}
