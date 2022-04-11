3ada7cd57ffe91ef68c57ab80757fde5e1b0817e
thaisgimenes <thaisgimenes@outlook.com.br>
2022-04-11 19:40:07 -0300

    Adicionando novas chaves com informação em um objeto

---
+++ adicionar-campos.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "4521336987",
+    email: "andre@email.com"
+}
+
+
+console.log(cliente);
+
+//Crindo uma nova chave com informação
+cliente.fone = "36548999";
+
+console.log(cliente);
